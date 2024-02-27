e6b4d49b6d629670430f5011aaa5a08c931f865c
talesofterror <mich.rivera84@gmail.com>
2024-02-27 10:37:14 -0500

    bash-scripts, arrays; empty output.sh

---
+++ bash-scripts/arrays.sh
@@ -0,0 +1,41 @@
+#bin/bash
+
+array1=("a" "b" "c" "d")
+
+echo ${array1[2]}
+# using $array1 would output only index 0
+
+echo ${array1[@]}
+# output all elements of the array
+
+for i in ${array1[@]}; do 
+	echo $i
+done
+# loops through the array where $i is the arrays elements
+
+for i in ${!array1[@]}; do 
+	echo ${array1[$i]}
+done
+# loops through the array where $i is the array index
+
+newArray1=()
+# declare a new empty array that can be populated
+
+for i in ${array1[@]}; do 
+	runtime=$(./output.sh --out $i) #hypothetical script that expects $i
+	# output this iteration goes into $runtime
+	newArray1+=( $runtime )
+	# and is used to populate $newArray1
+done
+# untested
+
+
+for i in ${array1[@]}; do 
+	echo $i
+done
+# returns element (no bang)
+
+for i in ${!array1[@]}; do 
+	echo $i
+done
+#returns index number (yes bang)
